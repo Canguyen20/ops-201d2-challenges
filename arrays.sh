962a6bf0e14f9fe31e68a7eaa05d44638e56ec9f
cgolob <carriegolob@gmail.com>
2021-01-28 22:17:41 +0000

    updates

---
+++ arrays.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script:  Arrays                     
+# Author:    Carrie Nguyen                
+# Date of latest revision:    1/28/20  
+# Purpose:      Ops Challenge 04                
+
+# Declare global variables
+animal_list=("dog.txt" "cat.txt" "bear.txt" "wolf.txt" "bird.txt")
+
+# Declare functions
+
+# Main
+echo ${animal_list[*]}

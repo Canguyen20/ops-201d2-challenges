5e0e71f1b1ebd8a24837d061516fc210e1d38e9e
cgolob <carriegolob@gmail.com>
2021-01-28 02:26:02 +0000

    added update

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Script Name:       Challenge 01
+#Class Name:        Ops 201
+#Aurthor Name:      Carrie Nguyen
+#Date of last revision:  01/27/21
+#Purpose:  Class Challenge
+
+#What is a variable:  A variable is like a bag.  It is empty until you put something in it
+
+#Declaration
+my_variable="Hello World"
+
+#Call
+echo $my_variable
+
